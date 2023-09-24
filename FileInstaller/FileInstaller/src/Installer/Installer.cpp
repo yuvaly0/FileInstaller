c112d1f5b52af5c17efaf8cee5233cb6537e3c4e
yuval <yuvalyohanan3@gmail.com>
2023-09-24 09:52:12 +0300

    redundant

--- FileInstaller/FileInstaller/src/Installer/Installer.cpp
+++ FileInstaller/FileInstaller/src/Installer/Installer.cpp
@@ -5,14 +5,7 @@
 Installer::Installer() {
 	_destinationPath = new DestinationPath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise");
 	_sourcePaths = {
-		new SourcePath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\1.txt"),
-		/*
-		new SourcePath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\2.txt"),
-		new SourcePath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\3.txt"),
-		new SourcePath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\4.txt"),
-		new SourcePath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\5.txt"),
-		new SourcePath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\6.txt")
-		*/
+		new SourcePath(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\1.txt")
 	};
 	_hasCreatedDestinationFolder = false;
 }
