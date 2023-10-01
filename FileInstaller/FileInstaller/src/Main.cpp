dacc20ae72ab3ac341150fdd41bfa25bba07bcb7
yuval <yuvalyohanan3@gmail.com>
2023-10-01 12:39:20 +0300

    update readme

--- FileInstaller/FileInstaller/src/Main.cpp
+++ FileInstaller/FileInstaller/src/Main.cpp
@@ -6,10 +6,10 @@ int main() {
 	
 	std::vector<std::shared_ptr<Action>> actions = {
 		std::make_shared<CreateDirectoryAction>(destinationPath),
-		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\1.txt", destinationPath),
-		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\bla", destinationPath),
-		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\bla2", destinationPath),
-		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\winapi exercise\\copyMe\\bla3", destinationPath)
+		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\FileInstaller\\copyMe\\1.txt", destinationPath),
+		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\FileInstaller\\copyMe\\bla", destinationPath),
+		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\FileInstaller\\copyMe\\bla2", destinationPath),
+		std::make_shared<CopyPathAction>(L"C:\\Users\\yuvalyo\\Documents\\Projects\\FileInstaller\\copyMe\\bla3", destinationPath)
 	};
 
 	auto installer = std::make_unique<Installer>(actions);
